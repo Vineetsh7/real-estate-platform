3ce71f9f9bba20571a72ef458ba3fb1dcecb38c5
Vineetsh7 <xyz.meoutlook010@gmail.com>
2025-12-23 23:37:15 +0530

    Add application routes and controller methods for application management

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,6 +12,7 @@ import managerRoutes from "./routes/managerRoutes";
 import tenantRoutes from "./routes/tenantRoutes";
 import propertyRoutes from "./routes/propertyRoutes";
 import leaseRoutes from "./routes/leaseRoutes";
+import applicationRoutes from "./routes/applicationRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -29,6 +30,7 @@ app.get("/", (req, res) => {
   res.send("This is home route");
 });
 
+app.use("/applications", applicationRoutes);
 app.use("/properties", propertyRoutes);
 app.use("/leases", leaseRoutes);
 app.use("/tenants", authMiddleware(["tenant"]), tenantRoutes);
